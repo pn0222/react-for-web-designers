a6e3214d4844f6759da586a00b91c00008a86da5
Paul Nguyen <pnguye40@uic.edu>
2018-05-04 21:46:56 +0000

    hw init

--- app.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  "use strict";
-
-  // Start here
-  var ProductCustomizer = React.createElement('div', {className: "customizer"}, "This is a customizer");
-  ReactDOM.render(ProductCustomizer, document.getElementById('react-root'));
-
-})();
