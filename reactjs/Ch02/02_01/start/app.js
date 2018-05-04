f60c888b07e26a85ac8bdfe6704e913d004b7a47
Paul Nguyen <pnguye40@uic.edu>
2018-05-04 22:13:29 +0000

    Start using JSX with Babel Help

--- reactjs/Ch02/02_01/start/app.js
+++ reactjs/Ch02/02_01/start/app.js
@@ -1,16 +1,26 @@
 (function() {
   "use strict";
+  
+  
 
   // Start here
   // var ProductCustomizer = React.createElement('div', {className: "customizer"}, "Hello World!");
   
   function ProductImage(props){
-    return React.createElement("img", {src:"../../../assets/red.jpg",alt:"Product image"});
+    // return React.createElement("img", {src:"../../../assets/red.jpg",alt:"Product image"});
+    return <img src="../../../assets/red.jpg" alt="Product image" />;
   }
   
   function ProductCustomizer(props){
-    return React.createElement("div", {className: "customizer"}, React.createElement("div", {className:"product-image"}, React.createElement(ProductImage)));
+    // return React.createElement("div", {className: "customizer"}, React.createElement("div", {className:"product-image"}, React.createElement(ProductImage)));
+    return(
+      <div className="customizer">
+        <div className="product-image">
+          <ProductImage/>
+        </div>
+      </div>
+    )
   }
   
-  ReactDOM.render(React.createElement(ProductCustomizer), document.getElementById("react-root"));
+  ReactDOM.render(<ProductCustomizer/>, document.getElementById("react-root"));
 })();
