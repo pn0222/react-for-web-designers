10c33e249097173fc3ecc7702487526572313f0c
Paul Nguyen <pnguye40@uic.edu>
2018-05-04 21:51:26 +0000

    Installing React in an existing site

--- Exercise Files/Ch02/02_01/start/app.js
+++ Exercise Files/Ch02/02_01/start/app.js
@@ -2,5 +2,6 @@
   "use strict";
 
   // Start here
-
+  var ProductCustomizer = React.createElement('div', {className: "customizer"}, "Hello World!");
+  ReactDOM.render(ProductCustomizer, document.getElementById('react-root'));
 })();
