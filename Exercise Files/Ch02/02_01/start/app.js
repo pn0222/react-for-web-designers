1426c4d11b19d2d9581600513c5a5462a1f1ab4a
Paul Nguyen <pnguye40@uic.edu>
2018-05-04 22:01:05 +0000

    Set up first simple component

--- Exercise Files/Ch02/02_01/start/app.js
+++ Exercise Files/Ch02/02_01/start/app.js
@@ -2,6 +2,15 @@
   "use strict";
 
   // Start here
-  var ProductCustomizer = React.createElement('div', {className: "customizer"}, "Hello World!");
-  ReactDOM.render(ProductCustomizer, document.getElementById('react-root'));
+  // var ProductCustomizer = React.createElement('div', {className: "customizer"}, "Hello World!");
+  
+  function ProductImage(props){
+    return React.createElement("img", {src:"../../../assets/red.jpg",alt:"Product image"});
+  }
+  
+  function ProductCustomizer(props){
+    return React.createElement("div", {className: "customizer"}, React.createElement("div", {className:"product-image"}, React.createElement(ProductImage)));
+  }
+  
+  ReactDOM.render(React.createElement(ProductCustomizer), document.getElementById("react-root"));
 })();
